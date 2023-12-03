40c34600f90f8915a2ef56c49444da584fd317b5
shivam-jainn <shivam.clgstash@gmail.com>
2023-12-03 15:44:27 +0530

    feat(src): :sparkles: added submission api
    This api executes and builds docker , and returns the O/P in json format
    
    n/a

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from "express";
+import * as execute from "./execute";
 
 const server: Express = express();
 const PORT = process.env.PORT as string || 3000;
@@ -7,13 +8,15 @@ server.use(express.json());
 
 server.post("/submission", async (req: Request, res: Response) => {
   const { userCode, problemNumber, language, userid } = req.body;
-
-//   const incoming = questions.findOne({ problemNumber: problemNumber });
-
-//   run_container(language, userCode, incoming);
-
- 
+  let output_json;
+  const container_id = await execute.default.main_exec(problemNumber, language);
+  if (container_id) {
+    output_json = await execute.default.waitForFile(container_id);
+  }
+  res.send(JSON.stringify(output_json)).status(200);
 });
+
+
 server.listen(PORT,()=>{
     console.log("PORT is "+PORT); 
 });
